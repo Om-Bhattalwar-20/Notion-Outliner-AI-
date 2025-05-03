80d2ee9b94eb3663a9b865c8b30fc643369a5e2f
Om205 <164125899+Om-Bhattalwar-20@users.noreply.github.com>
2025-05-03 22:46:18 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Templates from "./pages/Templates";
+
 import MindMap from "./pages/MindMap";
 
 import WritingCoach from "./pages/WritingCoach";
