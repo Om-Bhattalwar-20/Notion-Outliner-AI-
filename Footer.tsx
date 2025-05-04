e68647ca2cda2c1d507f90de3889ca65c934d1f9
Om205 <164125899+Om-Bhattalwar-20@users.noreply.github.com>
2025-05-04 20:33:26 +0530

    Update Footer.tsx

--- Footer.tsx
+++ Footer.tsx
@@ -2,6 +2,7 @@
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
+  
   return (
     <footer className="bg-notion-gray-100 dark:bg-notion-gray-700/20 py-12 mt-20">
       <div className="container mx-auto px-4">
