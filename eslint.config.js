e88cea5d1899ee6c3d1e996c38cedd46d6d66aa8
Om Bhattalwar <164125899+Om-Bhattalwar-20@users.noreply.github.com>
2025-05-05 18:27:41 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,7 @@ export default tseslint.config(
   {    
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
-    
+  
     languageOptions: {
 
       ecmaVersion: 2020,
