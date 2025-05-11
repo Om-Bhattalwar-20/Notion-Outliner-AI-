ed70139e1fb9b02d4d2fae23d843aad0e2437bd5
Om Bhattalwar <164125899+Om-Bhattalwar-20@users.noreply.github.com>
2025-05-11 19:58:47 +0530

    Update MindMapToggle.tsx

--- MindMapToggle.tsx
+++ MindMapToggle.tsx
@@ -42,6 +42,7 @@ const MindMapToggle = () => {
             </ToggleGroup>
           </div>
         </div>
+        
 
         <div className="max-w-4xl mx-auto border rounded-lg overflow-hidden shadow-lg">
           {viewMode === "linear" ? (
